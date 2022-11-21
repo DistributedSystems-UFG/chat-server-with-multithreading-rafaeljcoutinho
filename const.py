20c634f1ded2e93870d71613582cda10152e8f3d
Rafael Junqueira Coutinho <69867710+rafaeljcoutinho@users.noreply.github.com>
2022-11-21 14:56:07 -0300

    Update const.py

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
 CHAT_SERVER_HOST = "172.31.81.153"
-CHAT_SERVER_PORT = 5001
+CHAT_SERVER_PORT = 5680
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
 registry = {"Alice":("172.31.89.39",5002), "Bob":("172.31.90.253",5002)}
